0dd4d295d279a3b9a527b671810796e7ea179106
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-04-04 20:51:25 +0900

    Initial Commit

---
+++ pockemongetdaze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pockemongetdaze
+//
+//  Created by 安部学 on 2020/04/04.
+//  Copyright © 2020 Manabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
