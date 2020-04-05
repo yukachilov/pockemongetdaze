5e0ede14dafc38c7bc81408c45000121dd2f15c5
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-04-05 13:54:25 +0900

    入力中

--- pockemongetdaze/ViewController.swift
+++ pockemongetdaze/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchText.delegate = self
+        
+        searchText.placeholder = "ポケモンの名前を入力するのだ！"
+        
+        
     }
-
-
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            
+            searchPockemon(keyword: searchWord)
+        }
+        
+    }
+    
+    struct ItemJson: Codable {
+        
+        let no: Int?
+        
+        let name: String?
+        
+        let form: String?
+        
+        let isMegaEvolution: Bool?
+        
+        let evolutions: Int?
+        
+        let types: String?
+        
+        let abilities: String?
+        
+        let hiddenAbilities: String?
+        
+        var stats: Int?{
+            let hp: Int?
+            let attack: Int?
+            
+        }
+        
+    }
+    
+    
+    
+    func searchPockemon(keyword : String){
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        guard let req_url = URL(string: "https://github.com/kotofurumiya/pokemon_data/blob/master/data/pokemon_data.json") else {
+            return
+        }
+        print(req_url)
+    }
+    
+    
 }
 
